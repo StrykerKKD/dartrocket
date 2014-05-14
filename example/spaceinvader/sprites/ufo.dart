d41237d507af109e60cf0f38a6f49a9514ec4472
StrykerKKD <kkdstryker@gmail.com>
2014-05-14 17:53:59 +0200

    Ufo use tween for moving sideways

--- example/spaceinvader/sprites/ufo.dart
+++ example/spaceinvader/sprites/ufo.dart
@@ -1,22 +1,40 @@
 part of spaceinvader;
 
-class Ufo extends AnimatedSprite{
-  
-  Ufo(var bitmapData, {int x, int y, int vy: 30}): super(bitmapData, x: x,
-        y: y, vy: vy){
+class Ufo extends AnimatedSprite {
+
+  bool moveRight = true;
+  bool moving = false;
+  int deltaX;
+
+  Ufo(var bitmapData, {int x, int y, int vy: 30}): super(bitmapData, x: x, y: y,
+      vy: vy) {
     center();
     alive = true;
   }
-  
-    
+
+
   @override
   bool advanceTime(num time) {
     // TODO: implement advanceTime
-    if(y > game.stage.sourceHeight || alive == false){
+    if (y > game.stage.sourceHeight || alive == false) {
       game.stage.removeChild(this);
-      game.stage.juggler.remove(this);      
+      game.stage.juggler.remove(this);
     }
-    
+    if (!moving) {
+      if (moveRight) {
+        deltaX = 50;
+      } else {
+        deltaX = -50;
+      }
+      moving = true;
+      game.stage.juggler.tween(this, 1, TransitionFunction.easeInOutCubic)
+          ..animate.x.by(deltaX)
+          ..onComplete = () {
+            moveRight = !moveRight;
+            moving = false;
+          };
+
+    }
     y = y + vy * time;
   }
 }
