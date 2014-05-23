840bfe4fd8e14f2fd72fb4ded8b7e1ca620eec55
StrykerKKD <kkdstryker@gmail.com>
2014-05-23 19:17:05 +0200

    Modified Full sprite
    This sprite is interactive and animatable.
    This will be the basic Sprite in the next release.

--- lib/src/gameobject/full_sprite.dart
+++ lib/src/gameobject/full_sprite.dart
@@ -1,23 +1,66 @@
 part of dartrocket;
 
-abstract class FullSprite extends InteractiveSprite implements StageXL.Animatable{
+/**
+ * This interface is the most basic object.
+ * */
+
+class FullSprite extends InteractiveBitmap implements StageXL.Animatable {
+
+  bool isMoveAble;
+
   /**
-     * Horizontal speed of the sprite.
-     * */
-    num vx;
+   * Is the sprite alive?
+   * */
+  bool alive = false;
 
-    /**
-     * Vertical speed of the sprite
-     * */
-    num vy;
+  /**
+   * Horizontal speed of the sprite.
+   * */
+  num vx = 0;
 
-    /**
-     * Creates an FullSprite object.
-     * 
-     * * stateContext: context of the State, which this object is in
-     * * resourceName: name of the resource for the sprite
-     * 
-     * */
-    FullSprite(State stateContext, String resourceName) : super(stateContext,
-        resourceName);
+  /**
+   * Vertical speed of the sprite
+   * */
+  num vy = 0;
+
+  /**
+   * Creates a Sprite object.
+   * 
+   * * stateContext: context of the State, which this object is in
+   * * resourceName: name of the resource for the sprite
+   * 
+   * */
+  FullSprite(State stateContext, String resourceName, {bool addToStage:
+      true, bool isMoveAble: false}) : super(
+      stateContext.game.resourceManager.getBitmapData(resourceName)) {
+    this.isMoveAble = isMoveAble;
+    if(addToStage){
+      this.addToStage(stateContext);
+    }
+  }
+
+  bool advanceTime(num time) {
+    x = x - vx * time;
+    y = y - vy * time;
+  }
+
+  void center() {
+    pivotX = bitmapData.width / 2;
+    pivotY = bitmapData.height / 2;
+  }
+
+  addToStage(State context) {
+    context.game.stage.addChild(this);
+    if (isMoveAble) {
+      context.game.stage.juggler.add(this);
+    }
+  }
+  
+  killSprite(State context){
+    context.game.stage.removeChild(this);
+    if(isMoveAble){
+      context.game.stage.juggler.remove(this);
+    }
+  }
+
 }
