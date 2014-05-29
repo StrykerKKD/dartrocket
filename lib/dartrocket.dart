ba247ddf8bb4700b6bfb9b8ca06bcef1a93901ac
StrykerKKD <kkdstryker@gmail.com>
2014-05-29 19:14:55 +0200

    Reorganizing the project structure

--- lib/dartrocket.dart
+++ lib/dartrocket.dart
@@ -11,11 +11,13 @@ import "package:stagexl/stagexl.dart" as StageXL;
 part "src/core/game.dart";
 part "src/core/statemanager.dart";
 part "src/core/state.dart";
-part "src/core/background.dart";
-part "src/core/text.dart";
-
-part "src/animation/transition_function.dart";
+part "src/core/group.dart";
 
 part "src/gameobject/interactive_bitmap.dart";
 part "src/gameobject/sprite.dart";
-part "src/gameobject/group.dart";
+part "src/gameobject/background.dart";
+part "src/gameobject/text.dart";
+
+part "src/animation/transition_function.dart";
+
+
