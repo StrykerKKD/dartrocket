ad18d180270fd5e28c01e01569defca48f004169
StrykerKKD <kkdstryker@gmail.com>
2014-05-30 22:53:43 +0200

    Removed defTextFormat

--- lib/src/gameobject/text.dart
+++ lib/src/gameobject/text.dart
@@ -2,13 +2,11 @@ part of dartrocket;
 
 class Text extends StageXL.TextField {
 
-  final StageXL.TextFormat defTextFormat = new StageXL.TextFormat('Arial', 24,
-      StageXL.Color.Black, bold: true, italic: true);
-
   Text(text, [StageXL.TextFormat textformat]): super() {
     this.text = text;
     if (textformat == null) {
-      this.defaultTextFormat = defaultTextFormat;
+      this.defaultTextFormat = new StageXL.TextFormat('Arial', 14,
+          StageXL.Color.Black, bold: true, italic: true);
     }
   }
 }
