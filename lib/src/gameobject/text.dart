37bcdd8ac0e82137a9ae8f3fbee5973eb01bfe3d
StrykerKKD <kkdstryker@gmail.com>
2014-05-31 23:02:25 +0200

    Text can be automatically added

--- lib/src/gameobject/text.dart
+++ lib/src/gameobject/text.dart
@@ -1,12 +1,22 @@
 part of dartrocket;
 
 class Text extends StageXL.TextField {
-
-  Text(text, {String font: 'Arial', int size: 14, int color:
-      StageXL.Color.Black}) : super() {
+  State _context;
+  
+  Text(State context, String text, {String font: 'Arial', int size: 14, int color:
+      StageXL.Color.Black,bool addToStage:true}) : super() {
     
     this.text = text;
     
     this.defaultTextFormat = new StageXL.TextFormat(font, size, color);
+    
+    _context = context;
+    if(addToStage){
+      _context.game.stage.addChild(this);
+    }
+  }
+  
+  addToStage(){
+    _context.game.stage.addChild(this);
   }
 }
