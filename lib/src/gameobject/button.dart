aad6a844cd3c7da2b380dcb34e2e7d0bed477f21
StrykerKKD <kkdstryker@gmail.com>
2014-06-06 00:06:10 +0200

    Reconstructed Button class
    Extends interActiveBitmap instead of Sprite.
    Button label vertically aligned.

--- lib/src/gameobject/button.dart
+++ lib/src/gameobject/button.dart
@@ -1,24 +1,42 @@
 part of dartrocket;
 
-class Button extends Sprite {
+class Button extends InteractiveBitmap {
 
+  State _context;
   Text buttonText;
 
-  Button(State context, String resourceName,String text) : super(context, resourceName,
-      isMoveAble: false) {
-    buttonText = new Text(context, text)
-    ..width = width
-    ..height = height
-    ..defaultTextFormat.align = StageXL.TextFormatAlign.CENTER;       
+  Button(State context, String resourceName, String text, {bool addToStage:
+      true}) : super(context.game.resourceManager.getBitmapData(resourceName)) {
+    _context = context;
+
+    buttonText = new Text(context, text, addToStage: false)
+        ..width = width
+        ..height = height
+        ..defaultTextFormat.align = StageXL.TextFormatAlign.CENTER;
+    buttonText
+        ..defaultTextFormat.topMargin = (buttonText.height -
+            buttonText.textHeight) ~/ 2
+        ..text = text;
+
+    if (addToStage) {
+      this.addToStage();
+    }
+  }
+
+  addToStage() {
+    _context.game.stage.addChild(this);
+    _context.game.stage.addChild(buttonText);
+
   }
 
   void setCoordinates(int x, int y) {
     this.x = x;
     this.y = y;
     buttonText.x = x;
-    buttonText.y = y;    
+    buttonText.y = y;
   }
-  
-  StageXL.EventStream<StageXL.MouseEvent> get onMouseClick => StageXL.InteractiveObject.mouseClickEvent.forTarget(buttonText);
+
+  StageXL.EventStream<StageXL.MouseEvent> get onMouseClick =>
+      StageXL.InteractiveObject.mouseClickEvent.forTarget(buttonText);
 
 }
