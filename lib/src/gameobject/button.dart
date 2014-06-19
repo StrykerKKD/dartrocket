43a10512ed813a491cc8a6ac66fc0fffc1fac4f5
StrykerKKD <kkdstryker@gmail.com>
2014-06-19 21:36:32 +0200

    simplified button class

--- lib/src/gameobject/button.dart
+++ lib/src/gameobject/button.dart
@@ -27,7 +27,8 @@ class Button extends InteractiveBitmap {
     buttonText = new Text(context, text, addToStage: false)
         ..width = width
         ..height = height
-        ..defaultTextFormat.align = StageXL.TextFormatAlign.CENTER;
+        ..defaultTextFormat.align = StageXL.TextFormatAlign.CENTER
+        ..mouseEnabled = false;
     buttonText
         ..defaultTextFormat.topMargin = (buttonText.height -
             buttonText.textHeight) ~/ 2
@@ -38,41 +39,19 @@ class Button extends InteractiveBitmap {
     }
   }
 
+  set x(int x) {
+    super.x = x;
+    buttonText.x = x;
+  }
+  set y(int y) {
+    super.y = y;
+    buttonText.y = y;
+  }
+
   addToStage() {
     _context.game.stage.addChild(this);
     _context.game.stage.addChild(buttonText);
   }
-  /**
-   * Sets the coordinates of the button object.
-   * 
-   * **important:** Please use this for setting the coordinates.
-   */
-  void setCoordinates(int x, int y) {
-    this.x = x;
-    this.y = y;
-    buttonText.x = x;
-    buttonText.y = y;
-  }
-
-  /*
-   * We bind the eventlisteners to the buttonText object, because the buttonText
-   * is on top of the button.
-   */
-  StageXL.EventStream<StageXL.MouseEvent> get onMouseOver =>
-      StageXL.InteractiveObject.mouseOverEvent.forTarget(buttonText);
-  StageXL.EventStream<StageXL.MouseEvent> get onMouseDown =>
-      StageXL.InteractiveObject.mouseDownEvent.forTarget(buttonText);
-  StageXL.EventStream<StageXL.MouseEvent> get onMouseUp =>
-      StageXL.InteractiveObject.mouseUpEvent.forTarget(buttonText);
-  StageXL.EventStream<StageXL.MouseEvent> get onMouseClick =>
-      StageXL.InteractiveObject.mouseClickEvent.forTarget(buttonText);
-
-  StageXL.EventStream<StageXL.TouchEvent> get onTouchOver =>
-      StageXL.InteractiveObject.touchOverEvent.forTarget(buttonText);
-  StageXL.EventStream<StageXL.TouchEvent> get onTouchBegin =>
-      StageXL.InteractiveObject.touchBeginEvent.forTarget(buttonText);
-  StageXL.EventStream<StageXL.TouchEvent> get onTouchEnd =>
-      StageXL.InteractiveObject.touchEndEvent.forTarget(buttonText);
 
   State get context => _context;
 
