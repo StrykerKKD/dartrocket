4bf93568ba878f9315e1eb598b54b8772a68df22
StrykerKKD <kkdstryker@gmail.com>
2014-06-30 23:35:32 +0200

    using switch instead of if

--- lib/src/core/gameobjectfactory.dart
+++ lib/src/core/gameobjectfactory.dart
@@ -3,32 +3,35 @@ part of dartrocket;
 class GameObjectFactory {
 
   static const String IMAGE = 'image';
-    static const String TEXTUREATLAS = 'textureatlas';
-  
+  static const String TEXTUREATLAS = 'textureatlas';
+
   State currentContext;
   String defaultResourceMode = IMAGE;
   String defaultTextureAtlasName;
 
-  Sprite sprite(String resourceName, {String textureAtlasName,String resourceMode, bool addToStage:
-      true, bool isMovable: true}) {
+  Sprite sprite(String resourceName, {String textureAtlasName, 
+    String resourceMode, bool addToStage: true, bool isMovable: true}) {
 
     if (resourceMode == null) {
       resourceMode = defaultResourceMode;
     }
 
-    if (resourceMode == IMAGE) {
+    switch (resourceMode) {
       
-      return new Sprite.image(currentContext, resourceName, addToStage:
-          addToStage, isMovable: isMovable);
-    
-    } else if (resourceMode == TEXTUREATLAS) {
+      case IMAGE:
+        
+        return new Sprite.image(currentContext, resourceName, addToStage:
+            addToStage, isMovable: isMovable);        
       
-      if(textureAtlasName==null){
-        textureAtlasName = defaultTextureAtlasName;
-      }
-      
-      return new Sprite.textureatlas(currentContext, resourceName,
-          textureAtlasName, addToStage: addToStage, isMovable: isMovable);
+      case TEXTUREATLAS:
+        
+        if (textureAtlasName == null) {
+          textureAtlasName = defaultTextureAtlasName;
+        }
+
+        return new Sprite.textureatlas(currentContext, resourceName,
+            textureAtlasName, addToStage: addToStage, isMovable: isMovable);
+        
     }
 
   }
