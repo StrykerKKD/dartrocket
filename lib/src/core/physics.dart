671276bdd58fc06796b07b3f73fca27ccea624d8
StrykerKKD <kkdstryker@gmail.com>
2014-07-05 23:21:18 +0200

    make collison not based on an actual type

--- lib/src/core/physics.dart
+++ lib/src/core/physics.dart
@@ -1,18 +1,50 @@
 part of dartrocket;
 
 class Physics {
-  
-  void collison(Group group1, Group group2, Function collisonHandler(item1, itme2)){
-    
-    group1.forEachAlive((item1){
-      group2.forEachAlive((item2){
-        
-        if(item1.hitTestObject(item2)){
-          collisonHandler(item1,item2);
-        }
-        
+
+  void collison(var object1, var object2, Function
+      collisonHandler(item1, itme2)) {
+
+    if (object1 is Group && object1 is Group) {
+
+      object1.forEachAlive((item1) {
+        object2.forEachAlive((item2) {
+
+          if (item1.hitTestObject(item2)) {
+            collisonHandler(item1, item2);
+          }
+
+        });
       });
-    });
+
+    } else if (object1 is Sprite && object2 is Group) {
+
+      object2.forEachAlive((item) {
+
+        if (item.hitTestObject(object1)) {
+          collisonHandler(object1, item);
+        }
+
+      });
+
+    } else if (object1 is Group && object2 is Sprite) {
+
+      object1.forEachAlive((item) {
+
+        if (item.hitTestObject(object2)) {
+          collisonHandler(object2, item);
+        }
+
+      });
+
+    } else if (object1 is Sprite && object2 is Sprite) {
+
+      if (object1.hitTestObject(object2)) {
+        collisonHandler(object1, object2);
+      }
+
+    }
+
   }
-  
+
 }
