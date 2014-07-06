e0f933c279f79ce1c2cbbf475165d70f5dd2d738
StrykerKKD <kkdstryker@gmail.com>
2014-07-06 22:27:14 +0200

    add doc, fix the logic

--- lib/src/core/physics.dart
+++ lib/src/core/physics.dart
@@ -1,11 +1,21 @@
 part of dartrocket;
 
 class Physics {
-
+  
+  /**
+   * Resolve collison between two gameobject.
+   * 
+   * The input can be Group, Sprite or custome Sprite.
+   * 
+   * collisonHandler runs, when two object hits each other.
+   * The two actual object in collison is the input parameters 
+   * for the collisonHandler.
+   * 
+   */
   void collison(var object1, var object2, Function
       collisonHandler(item1, itme2)) {
 
-    if (object1 is Group && object1 is Group) {
+    if (object1 is Group && object2 is Group) {
 
       object1.forEachAlive((item1) {
         object2.forEachAlive((item2) {
@@ -32,7 +42,7 @@ class Physics {
       object1.forEachAlive((item) {
 
         if (item.hitTestObject(object2)) {
-          collisonHandler(object2, item);
+          collisonHandler(item, object2);
         }
 
       });
