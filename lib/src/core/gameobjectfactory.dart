b0a82812826f19baf69174246716aace31512c1f
StrykerKKD <kkdstryker@gmail.com>
2014-07-14 22:54:06 +0200

    GOF reworked: you can make any game object except Background.color
    added _getBitmaddata method, which made GOF  more readable
    you can make almost every game object with GOF

--- lib/src/core/gameobjectfactory.dart
+++ lib/src/core/gameobjectfactory.dart
@@ -12,89 +12,109 @@ class GameObjectFactory {
   /**
    * Creates a sprite from image or texture atlas.
    */
-  Sprite sprite(String resourceName, {String textureAtlasName, 
-    String resourceMode, bool addToStage: true, bool isMovable: true}) {
+  Sprite sprite(String resourceName, 
+                {String textureAtlasName: null, String resourceMode: null, 
+                 bool addToStage: true, bool isMovable: true}) {
+    
+    return new Sprite.bitmapdata(currentContext, 
+        _getBitmapData(resourceName,resourceMode,textureAtlasName),
+        addToStage:addToStage, isMovable: isMovable);
 
-    if (resourceMode == null) {
-      resourceMode = defaultResourceMode;
-    }
-
-    switch (resourceMode) {
+  }
+  
+  /**
+   * Creates multiple sprite from image or texture atlas.
+   */
+  List<Sprite> multipleSprite(int quantity, String resourceName, 
+      {String textureAtlasName: null, String resourceMode: null, 
+       bool addToStage: true, bool isMovable: true}){
       
-      case IMAGE:
-        
-        return new Sprite.image(currentContext, resourceName, addToStage:
-            addToStage, isMovable: isMovable);        
+    List<Sprite> _returnSpriteList = new List<Sprite>();
       
-      case TEXTUREATLAS:
-        
-        if (textureAtlasName == null) {
-          textureAtlasName = defaultTextureAtlasName;
-        }
-
-        return new Sprite.textureatlas(currentContext, resourceName,
-            textureAtlasName, addToStage: addToStage, isMovable: isMovable);
-        
+    for (int i;i < quantity;i++) {
+      _returnSpriteList.add(sprite(resourceName, textureAtlasName: textureAtlasName, 
+                                   resourceMode: resourceMode, addToStage: addToStage,
+                                   isMovable: isMovable));
     }
-
+      
+      return _returnSpriteList;
+      
   }
   
   /**
    * Create a button from image or texture atlas.
    */
   Button button(String resourceName, String text, 
-                {String textureAtlasName, String resourceMode,
+                {String textureAtlasName: null, String resourceMode: null,
                  bool addToStage: true}) {
     
-    if (resourceMode == null) {
-      resourceMode = defaultResourceMode;
-    }
+    return new Button.bitmapdata(currentContext, 
+        _getBitmapData(resourceName,resourceMode,textureAtlasName),text,
+        addToStage: addToStage);
     
+  }
+  
+  /**
+   * Create a text.
+   */
+  Text text(String text, 
+            {String font: 'Arial', int size: 14, int color: StageXL.Color.Black,
+             bool addToStage:true}) {
+    
+    return new Text(currentContext,text,font:font, size:size,color:color, 
+        addToStage: addToStage);
+  }
+  
+  /**
+   * Create a background from image or texture atlas.
+   * 
+   * **No color constructor support**
+   */
+  Background background(String resourceName, 
+                        {String textureAtlasName: null, String resourceMode: null,
+                          bool isMovable: false, bool addToStage: true, 
+                          String repeatMode: null, String scaleMode: null}) {
+    
+    return new Background.bitmapdata(currentContext, 
+        _getBitmapData(resourceName, resourceMode, textureAtlasName),
+        isMovable: isMovable, addToStage: addToStage, 
+        repeatMode: repeatMode, scaleMode: scaleMode);
+    
+  }
+  
+  /**
+   * Create a sound.
+   */
+  Sound sound(String soundName,{num volume:1,num pan:0}) {
+    return new Sound(currentContext, soundName, volume: volume, pan: pan);
+  }
+  
+  
+  StageXL.BitmapData _getBitmapData(String resourceName,
+                                    [String resourceMode,
+                                     String textureAtlasName]){
+    
+    if (resourceMode == null) {
+          resourceMode = defaultResourceMode;
+    }
+        
     switch (resourceMode) {
-      
+          
       case IMAGE:
-        
-        return new Button.image(currentContext, resourceName, text,
-            addToStage: addToStage);
-      
+            
+        return currentContext.game.resourceManager.getBitmapData(resourceName);
+          
       case TEXTUREATLAS:
-        
+            
         if (textureAtlasName == null) {
           textureAtlasName = defaultTextureAtlasName;
         }
         
-        return new Button.textureatlas(currentContext, resourceName, 
-            textureAtlasName, text,addToStage: addToStage);
-    }
-    
-    
-  }
+        return currentContext.game.resourceManager
+            .getTextureAtlas(textureAtlasName).getBitmapData(resourceName);               
+            
+    }  
   
-  
-  /**
-   * Creates multiple sprite from image or texture atlas.
-   */
-  List<Sprite> multipleSprite(int quantity, String resourceName, 
-      {String textureAtlasName, String resourceMode, 
-       bool addToStage: true, bool isMovable: true}){
-    
-    List<Sprite> _returnSpriteList = new List<Sprite>();
-    
-    if (resourceMode == null) {
-      resourceMode = defaultResourceMode;
-    }
-    
-    if (textureAtlasName == null) {
-      textureAtlasName = defaultTextureAtlasName;
-    }
-    
-    for (int i;i < quantity;i++) {
-      _returnSpriteList.add(sprite(resourceName, textureAtlasName: textureAtlasName, 
-                             resourceMode: resourceMode, addToStage: addToStage,
-                             isMovable: isMovable));
-    }
-    
-    return _returnSpriteList;
-  }
+  }  
   
 }
