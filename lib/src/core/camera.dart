7aa46a40b879a4140aac91d125303cb31e621b49
StrykerKKD <kkdstryker@gmail.com>
2014-07-21 23:19:04 +0200

    follow method added

--- lib/src/core/camera.dart
+++ lib/src/core/camera.dart
@@ -7,4 +7,11 @@ class Camera extends StageXL.Sprite {
   Camera(this._game, this._world){
     _game.stage.addChild(this);
   }
+  
+  void follow(Sprite sprite){
+    if(_world.contains(sprite)){
+      _world.removeChild(sprite);
+      addChild(sprite);
+    }
+  }
 }
