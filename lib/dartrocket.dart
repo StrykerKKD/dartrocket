aaee1b36e03e9f429e7bda2341452dc2946f5ccd
StrykerKKD <kkdstryker@gmail.com>
2014-07-24 23:17:32 +0200

    adding Keyboar class with checkKey method

--- lib/dartrocket.dart
+++ lib/dartrocket.dart
@@ -4,8 +4,8 @@ export "dart:html" show KeyCode;
 export "package:stagexl/stagexl.dart" show TransitionFunction, Color, TextureAtlasFormat;
 
 import "dart:async";
-import "dart:html" show window, document, CanvasElement;
-import 'dart:collection' show ListBase;
+import "dart:collection" show ListBase;
+import "dart:html" show window, document, CanvasElement, KeyCode;
 import "package:stagexl/stagexl.dart" as StageXL;
 
 part "src/core/game.dart";
@@ -23,5 +23,7 @@ part "src/gameobject/background.dart";
 part "src/gameobject/text.dart";
 part "src/gameobject/sound.dart";
 part "src/gameobject/button.dart";
+
+part "src/input/keyboard.dart";
 
 part "src/constants/event.dart";
---
+++ lib/src/input/keyboard.dart
@@ -0,0 +1,22 @@
+part of dartrocket;
+
+class Keyboard {
+
+  Game _game;
+
+  Keyboard(this._game);
+
+  void checkKey(keyCode, Function downHandler, Function upHandler) {
+    _game.world.onKeyDown.listen((keyboarEvent) {
+      if (keyboarEvent.keyCode == keyCode) {
+        downHandler();
+      }
+    });
+
+    _game.world.onKeyUp.listen((keyboarEvent) {
+      if (keyboarEvent.keyCode == keyCode) {
+        upHandler();
+      }
+    });
+  }
+}
