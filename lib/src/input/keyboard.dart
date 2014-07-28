e0383cbe150cfebdee65ca5e56dcafce434e64bb
StrykerKKD <kkdstryker@gmail.com>
2014-07-28 23:29:18 +0200

    idea: isDown handler for keys

--- lib/src/input/keyboard.dart
+++ lib/src/input/keyboard.dart
@@ -4,9 +4,13 @@ class Keyboard {
 
   Game _game;
 
+  Map<int, bool> _isDownMap = new Map<int, bool>();
+
   Keyboard(this._game);
 
-  void onDownAndUpKeyHandler(keyCode, Function downHandler, Function upHandler) {
+
+  void onDownAndUpKeyHandler(int keyCode, Function downHandler,
+      Function upHandler) {
     _game.world.onKeyDown.listen((keyboarEvent) {
       if (keyboarEvent.keyCode == keyCode) {
         downHandler();
@@ -16,6 +20,28 @@ class Keyboard {
     _game.world.onKeyUp.listen((keyboarEvent) {
       if (keyboarEvent.keyCode == keyCode) {
         upHandler();
+      }
+    });
+  }
+
+  bool isDown(int keyCode) {
+    if (!_isDownMap.containsKey(keyCode)) {
+      _addIsDownListener(keyCode);
+    }
+    return _isDownMap[keyCode];
+  }
+
+  void _addIsDownListener(int keyCode) {
+    _isDownMap[keyCode] = false;
+    _game.world.onKeyDown.listen((keyboarEvent) {
+      if (keyboarEvent.keyCode == keyCode) {
+        _isDownMap[keyCode] = true;
+      }
+    });
+
+    _game.world.onKeyUp.listen((keyboarEvent) {
+      if (keyboarEvent.keyCode == keyCode) {
+        _isDownMap[keyCode] = false;
       }
     });
   }
