a98e1b6d1e4b2dbd08a1c3e292f09427cb4a2d17
StrykerKKD <kkdstryker@gmail.com>
2014-08-14 22:52:51 +0200

    add doc and nextState for play state

--- example/projecttemplate/mygame.dart
+++ example/projecttemplate/mygame.dart
@@ -5,10 +5,14 @@ import 'package:dartrocket/dartrocket.dart';
 part 'play.dart';
 
 void main() {
+  
+  //starting the game engine
   Game game = new Game();
   
+  //adding the play state to the statemanager
   game.stateManager.addState('play', new Play('play'));
   
+  //start the play state
   game.stateManager.initState('play');
 }
 
--- example/projecttemplate/play.dart
+++ example/projecttemplate/play.dart
@@ -1,11 +1,17 @@
 part of mygame;
 
 class Play extends State {
-  Play(String name) : super(name);
+  Play(String name,[String nextState]) : super(name,nextState);
 
-  load() {}
+  load() {
+    //loading resources here(image,sound, textureatlas etc..)
+  }
 
-  create() {}
+  create() {
+    //create gameobjects and event handlers  
+  }
   
-  update() {}
+  update() {
+    //game loop: usually for handling physics and input events(moving the player)
+  }
 }
