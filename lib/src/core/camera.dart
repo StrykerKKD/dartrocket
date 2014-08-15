ec98fb47e00f9fa23abc4ff0c158269ef87d2fcd
StrykerKKD <kkdstryker@gmail.com>
2014-08-15 22:01:35 +0200

    implementing camera moving

--- lib/src/core/camera.dart
+++ lib/src/core/camera.dart
@@ -4,22 +4,29 @@ part of dartrocket;
  * Camera can change the view of the world.
  */
 class Camera extends StageXL.DisplayObjectContainer {
-  
+
   Game _game;
-  
+
   World _world;
-  
-  Camera(this._game, this._world){
+
+  Camera(this._game, this._world) {
     _game.stage.addChild(this);
   }
-  
-  /**
-   * Following a sprite object.
-   */
-  void follow(Sprite sprite){
-    if(_world.contains(sprite)){
-      _world.removeChild(sprite);
-      addChild(sprite);
+
+  void move(String direction, int distance) {
+    switch (direction) {
+      case Direction.UP:
+        _world.y += distance;
+        break;
+      case Direction.DOWN:
+        _world.y -= distance;
+        break;
+      case Direction.LEFT:
+        _world.x += distance;
+        break;
+      case Direction.RIGHT:
+        _world.x -= distance;
+        break;
     }
   }
 }
