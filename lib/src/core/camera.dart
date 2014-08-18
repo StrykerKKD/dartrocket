89e15491200960ad0ec148c4a12386dc252f9ea8
StrykerKKD <kkdstryker@gmail.com>
2014-08-18 22:38:27 +0200

    camera can follow sprite object and has a new update method

--- lib/src/core/camera.dart
+++ lib/src/core/camera.dart
@@ -9,11 +9,39 @@ class Camera extends StageXL.DisplayObjectContainer {
 
   World _world;
 
+  bool _following = false;
+
+  Sprite _targetSprite;
+
+  num _targetWorldX;
+
+  num _targetWorldY;
+
+  /**
+   * Widht of the world.
+   */
+  num width;
+
+  /**
+   * Height of the world.
+   */
+  num height;
+
+  StageXL.Point focusPoint;
+
   Camera(this._game, this._world) {
     _game.stage.addChild(this);
+
+    width = _game.stage.sourceWidth;
+    height = _game.stage.sourceHeight;
+
+    pivotX = width ~/ 2;
+    pivotY = height ~/ 2;
+    print('pivot: $pivotX, $pivotY');
+    //focusPoint = new StageXL.Point(pivotX, pivotY);
   }
 
-  void move(String direction, int distance) {
+  void moveDistance(String direction, {int distance: 5}) {
     switch (direction) {
       case Direction.UP:
         _world.y += distance;
@@ -29,9 +57,31 @@ class Camera extends StageXL.DisplayObjectContainer {
         break;
     }
   }
-  
+
+
+
+
   void follow(Sprite target) {
-    
+
+    _targetSprite = target;
+    _following = true;
+
   }
-  
+
+  void update() {
+    if (_following) {
+      _targetWorldX = _world.x + _targetSprite.x;
+      _targetWorldY = _world.y + _targetSprite.y;
+
+      if (_targetWorldX != pivotX || _targetWorldY != pivotY) {
+
+        num diffX = pivotX - _targetWorldX;
+        num diffY = pivotY - _targetWorldY;
+
+        _world.x += diffX;
+        _world.y += diffY;
+      }
+    }
+  }
+
 }
