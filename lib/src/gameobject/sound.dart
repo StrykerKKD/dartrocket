262989c64383a7de3ad74d3eab8f2040c79826c6
StrykerKKD <kkdstryker@gmail.com>
2014-11-25 21:26:25 +0100

    Sound can be stopped and made sound property private.

--- lib/src/gameobject/sound.dart
+++ lib/src/gameobject/sound.dart
@@ -4,34 +4,37 @@ part of dartrocket;
  * This class represent a Sound.
  */
 class Sound {
-  
-  /**
-   * The sound itself is stored at this.
-   */
-  StageXL.Sound sound;
-  
+
+  StageXL.Sound _sound;
+
+  StageXL.SoundChannel _soundchannel;
+
   /**
    * Setting for Sound(volume, pan).
    */
   StageXL.SoundTransform settings;
-  
+
 //=============================================================================
-  
+
   /**
    * Makes a Sound object.
    */
   Sound(State context, String soundName,{num volume:1,num pan:0}){
-    sound = context.game.resourceManager.getSound(soundName);
+    _sound = context.game.resourceManager.getSound(soundName);
     settings = new StageXL.SoundTransform(volume,pan);
   }
-  
+
 //=============================================================================
-  
+
   /**
    * Play the sound.
    */
   void play(){
-    sound.play(false, settings);
+    _soundchannel = _sound.play(false, settings);
   }
-  
+
+  void stop() {
+    _soundchannel.stop();
+  }
+
 }
