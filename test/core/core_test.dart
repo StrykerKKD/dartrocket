1152dda5475cf84a7ab73fb7759828176a641b4f
StrykerKKD <kkdstryker@gmail.com>
2015-03-15 22:55:16 +0100

    Add instance of and default settings tests.

--- test/core/core_test.dart
+++ test/core/core_test.dart
@@ -1,10 +1,20 @@
 library test.core;
 
 import 'package:unittest/unittest.dart';
+import 'dart:html' show CanvasElement;
 
+//=============================================================================
+
+import 'package:stagexl/stagexl.dart' as StageXL;
 import 'package:dartrocket/src/core.dart';
+import 'package:dartrocket/src/physics.dart' show PhysicsSystem;
+import 'package:dartrocket/src/input.dart';
+
+//=============================================================================
 
 part 'game_test.dart';
+
+//=============================================================================
 
 void main() {
   gameTest();
--- test/core/game_test.dart
+++ test/core/game_test.dart
@@ -1,5 +1,33 @@
 part of test.core;
 
 gameTest() {
-  
+  group("Game constructor", () {
+    Game game;
+    setUp((){
+      game = new Game();
+    });
+    tearDown((){
+      game = null;
+    });
+    test("Instance of", () {
+      expect(game.canvas, new isInstanceOf<CanvasElement>());
+
+      expect(game.stage, new isInstanceOf<StageXL.Stage>());
+      expect(game.renderLoop, new isInstanceOf<StageXL.RenderLoop>());
+      expect(game.resourceManager, new isInstanceOf<StageXL.ResourceManager>());
+
+      expect(game.stateManager, new isInstanceOf<StateManager>());
+      expect(game.add, new isInstanceOf<GameObjectFactory>());
+      expect(game.physics, new isInstanceOf<PhysicsSystem>());
+      expect(game.world, new isInstanceOf<World>());
+      expect(game.camera, new isInstanceOf<Camera>());
+      expect(game.keyboard, new isInstanceOf<Keyboard>());
+      expect(game.touch, new isInstanceOf<Touch>());
+    });
+    test("Default settings", () {
+      expect(game.canvas.attributes["screencanvas"] == "true", isTrue);
+      expect(game.canvas.width, equals(Game.DEFAULT_WIDTH));
+      expect(game.canvas.height, equals(Game.DEFAULT_HEIGHT));
+    });
+  });
 }
