67666001a0f224e13bd1b261b1119cc878c12de0
StrykerKKD <kkdstryker@gmail.com>
2015-03-25 22:13:14 +0100

    Simpler constructor.

--- lib/src/core/world.dart
+++ lib/src/core/world.dart
@@ -2,39 +2,37 @@ part of dartrocket.core;
 
 /**
  * The world is where almmost all of the game objects live.
- * 
+ *
  * The world object is made automatically when you make a game object.
  */
 class World extends StageXL.DisplayObjectContainer {
-  
+
   Game _game;
-  
+
 //=============================================================================
-  
+
   /**
    * Widht of the world.
    */
   num width;
-  
+
   /**
    * Height of the world.
    */
   num height;
-  
+
   /**
    * [StageXL.Juggler] object of the world.
-   * 
+   *
    * Uses the stage's juggler object.
    */
   StageXL.Juggler juggler;
-  
+
 //=============================================================================
-  
-  World(this._game, width, height) {
+
+  World(this._game, this.width, this.height) {
     juggler = _game.stage.juggler;
-    this.width = width;
-    this.height = height;
-    
-    _game.stage.addChild(this);    
+
+    _game.stage.addChild(this);
   }
 }
